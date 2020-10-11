b8cbacde8958f2f60e7b3a89209ad162934a1513
GrSkiy <43372592+GrSkiy@users.noreply.github.com>
2020-10-12 01:07:22 +0300

    Keyboard create, css reset

---
+++ app/javascript/components/controls/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class Button extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    window.addEventListener('ommouseup', this.props.handleUp)
+    window.addEventListener('keydown', this.props.keyDown)
+    window.addEventListener('keyup', this.props.keyUp)
+  }
+
+  render() {
+    return (
+      <div
+        id={'PianoKey' + this.props.buttonId}
+        className={this.props.classes.join(' ')}
+        onMouseDown={this.props.handleDown}
+        onMouseUp={this.props.handleUp}
+      ></div>
+    )
+  }
+}
