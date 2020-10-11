d5065992e786649848851577041f5ba2fda1b84d
ALeo75C <za.iz.pr@yandex.kz>
2020-10-11 14:40:20 +0300

    добавила синт, возможность добавление новых синтов, исправить изменение ноты

--- app/javascript/components/instruments/ToneSynth.jsx
+++ app/javascript/components/instruments/ToneSynth.jsx
@@ -1,12 +1,98 @@
 import React from 'react'
 import * as Tone from 'tone'
 
+import { notes } from '../../utilities/notes'
+
 export default class ToneSynth extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      band: []
+    }
+  }
+
+  componentDidMount() {
+    // this.setInstrument()
+  }
+
+  handleCreateSynth = () => {
+    let { band } = this.state
+    let synth = new Tone.PolySynth().toDestination()
+
+    band.push({ instrument: synth, isPlaying: false, note: 'C4' })
+
+    this.setState({
+      band
+    })
+  }
+
+  renderSynths = () => {
+    const { band } = this.state
+    let synths = []
+    if (band.length != 0) {
+      band.forEach((synth, i) => {
+        synths.push(
+          <div className="Synth" key={`${i}_synth`}>
+            <h1>Synth {i}</h1>
+            <div onClick={() => this.handleSynthTogglePlay(i)} key={i}>
+              {synth.isPlaying ? 'Stop' : 'Start'}
+            </div>
+            <div key={`${i}_notes`}>{this.renderNotes(i)}</div>
+            _____________________________________________________
+          </div>
+        )
+      })
+      return synths
+    }
+  }
+
+  handleChangeNote = (id, note) => {
+    const { band } = this.state
+    band[id].note = note
+    this.setState({
+      band
+    })
+  }
+
+  renderNotes = (id) => {
+    let notesItems = []
+    Object.keys(notes).forEach((note, i) => {
+      notesItems.push(
+        <span onClick={() => this.handleChangeNote(id, `${note}4`)} key={i}>
+          | {`${note}4`} ||
+        </span>
+      )
+    })
+    return notesItems
+  }
+
+  handleSynthTogglePlay = (id) => {
+    const { band } = this.state
+
+    if (band[id].isPlaying) {
+      band[id].instrument.triggerRelease(band[id].note)
+      band[id].isPlaying = false
+      this.setState({
+        band
+      })
+    } else {
+      band[id].instrument.triggerAttack(band[id].note)
+      band[id].isPlaying = true
+      this.setState({
+        band
+      })
+    }
   }
 
   render() {
-    return <div className="Synth">Synth</div>
+    return (
+      <div>
+        {this.renderSynths()}
+        <div onClick={this.handleCreateSynth} className="Synth">
+          Create Synth
+        </div>
+      </div>
+    )
   }
 }
--- app/javascript/packs/synth.jsx
+++ app/javascript/packs/synth.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ADCSynth from '../containers/ADCSynth'
+import ToneSynth from '../components/instruments/ToneSynth'
 
 document.addEventListener('DOMContentLoaded', () => {
   // const props = JSON.parse(document.getElementById('data').dataset.props)
 
+  // <ADCSynth />,
   ReactDOM.render(
-    <ADCSynth />,
+    <ToneSynth />,
     document.body.appendChild(document.createElement('div'))
   )
 })
