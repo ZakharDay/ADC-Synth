077f7fa65458cff5f763561e9af34e3a5e768407
ALeo75C <za.iz.pr@yandex.kz>
2020-10-24 00:22:33 +0300

    WIP

--- app/javascript/components/parts/Channel.jsx
+++ app/javascript/components/parts/Channel.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 
+import InstrumentMenu from './InstrumentMenu'
+
 export default class Channel extends React.Component {
   constructor(props) {
     super(props)
   }
 
   render() {
-    return <div className="Channel">Channel</div>
+    let buttons = [
+      {
+        name: 'Synth',
+        isOn: false,
+        handleClick: () => console.log('Channels click')
+      },
+      {
+        name: 'Sampler',
+        isOn: false,
+        handleClick: () => console.log('Channels click')
+      }
+    ]
+    return (
+      <div className="Channel">
+        <InstrumentMenu
+          name="channels"
+          open={() => console.log('Channels click')}
+          buttons={buttons}
+        />
+      </div>
+    )
   }
 }
